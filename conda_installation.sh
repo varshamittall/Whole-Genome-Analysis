30b6b836d4616f090e07eecf32db612edb70a7b7
Varsha Mittal <124229435+varshamittall@users.noreply.github.com>
2023-09-18 11:03:56 +0530

    Add files via upload

---
+++ conda_installation.sh
@@ -0,0 +1,5 @@
+#install conda 
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_22.11.1-1-Linux-x86_64.sh
+chmod +x Miniconda3-py39_22.11.1-1-Linux-x86_64.sh
+bash Miniconda3-py39_22.11.1-1-Linux-x86_64.sh 
+conda --version
